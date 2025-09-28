ab557b55170e3695cb2f39d15342f56610223786
Layba-Zahid <Laybazahid22@gmail.com>
2025-09-28 19:33:52 +0500

    Added .gitignore and removed node_modules

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState , createContext} from 'react'
+
+import CurrencyConvertor2 from './Components/CurrencyConvertor2'
+
+
+function App() {
+
+
+
+
+
+
+ return(
+
+ 
+
+<CurrencyConvertor2/>
+
+
+
+
+
+
+
+
+ 
+ );
+
+}
+    
+
+  
+
+
+export default App;
---
+++ src/Components/CurrencyConvertor2.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import { useState , useEffect} from 'react'
+import "./CurrencyConvertor2.css"
+import swapIcon from "../assets/swap.png"
+
+const CurrencyConvertor2 = () => {
+
+const [Amount, setAmount] = useState("")
+const [FromCurrency, setFromCurrency] = useState("USD")
+const [ToCurrency, setToCurrency] = useState("PKR")
+const [Rates, setRates] = useState({})
+const [Result, setResult] = useState(null)
+
+
+useEffect(() => {
+  fetch("https://api.exchangerate-api.com/v4/latest/USD")
+     .then(res=>res.json())
+     .then(data=>setRates(data.rates))
+}, [])
+
+
+const handleConvertor=(e)=>{
+if(!Amount || ! Rates[ToCurrency]) return;
+ e.preventDefault();
+const converted= Number( Amount )/ Rates[FromCurrency] * Rates[ToCurrency];
+setResult(converted);
+}
+
+
+const handleSwap = () => {
+  const temp = FromCurrency;
+  setFromCurrency(ToCurrency);
+  setToCurrency(temp);
+};
+
+  return (
+    <div>
+      <div className='container' >
+       <div className="inner-box">
+
+        <div className="heading">
+        <h2>Currency Convertor</h2>
+        <p>Convert currencies with real-time exchange rates</p>
+        </div>
+
+        <form>
+            <label htmlFor="Amount">Amount</label>
+             <input type='Text' id='Amount' placeholder='Enter Amount' value={Amount}  onChange={(e)=>setAmount(e.target.value)}></input>
+
+
+             <label className='From' htmlFor="From">From</label>
+          <select  type="text" id='From' value={FromCurrency} onChange={(e)=>setFromCurrency(e.target.value)} >
+           {Object.entries(Rates).map(([ Currency , value])=>(
+            <option key={Currency} value={Currency}>
+            {Currency}
+            </option>
+           ))}
+          </select>
+
+
+          <button type="button" onClick={handleSwap} className="swap-btn">
+  <img src={swapIcon} alt='swap' width="40" height="40"></img>
+</button>
+
+
+            <label className='To' htmlFor="To">To</label>
+          <select  type="text"  id="To" value={ToCurrency} onChange={(e)=>setToCurrency(e.target.value)}>
+          {Object.entries(Rates).map(([currency,value])=>(
+           <option key={currency} value={currency}>
+            {currency}
+           </option>
+          ))}
+           </select>
+
+
+
+          <button  className="btn1" onClick={handleConvertor}>Convert</button>
+    
+           {Result && (
+        <h3>
+         {Amount} {FromCurrency}={Result} {ToCurrency}
+         </h3>
+      )
+
+      }
+      
+
+
+        </form>
+
+
+
+       </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default CurrencyConvertor2
